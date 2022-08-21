0356c3bb307261b4dec71d5919e49e15c9a0f99e
AntonAscheulov <schuca120@mail.ru>
2022-08-21 16:10:08 +0300

    diploma ver.1.13

--- resources/views/mails/contact.blade.php
+++ resources/views/mails/contact.blade.php
@@ -1,1 +1,33 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sing Up Notification</title>
+    <link rel="stylesheet" href="/css/front.css">
+</head>
+<body>
+<div class="container">
+    <div class="card" style="width: 18rem;">
+        <div class="card-header">
+            <h2>Hey {{$admin->name}}!</h2>
+<br>
+<p>New User is join us!</p>
+</div>
+<ul class="list-group list-group-flush">
+    <li class="list-group-item font-weight-bolder"> User name:</li>
+    <li class="list-group-item"> {{$contact->name}}</li>
+    <li class="list-group-item font-weight-bolder" >User email:</li>
+    <li class="list-group-item">{{$contact->email}}</li>
+    <li class="list-group-item font-weight-bolder" >Subject:</li>
+    <li class="list-group-item">{{$contact->subject}}</li>
+    <li class="list-group-item font-weight-bolder" >Message:</li>
+    <li class="list-group-item">{{$contact->message}}</li>
+</ul>
+</div>
+</div>
+<script src="/js/front.js"></script>
+</body>
+</html>
